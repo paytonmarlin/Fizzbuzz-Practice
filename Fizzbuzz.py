f95a7451c34ca6d70581754e04bdaae02281cdcd
PRunner77 <pjmddc@mail.missouri.edu>
2021-03-09 12:10:50 -0600

    added comments to code

--- Fizzbuzz.py
+++ Fizzbuzz.py
@@ -1,14 +1,18 @@
 def main():
-    l = 100
-    for i in range(l):
-        if i % 5 == 0 and i % 3 == 0:
+    end = 100 #Defined a variable to hold the maximum number that we want to fizzbuzz
+
+    #A for loop to iterate over every number until we hit the targeted variable above
+    for num in range(end):
+        #Some logic pertaining if the number is divisible by 3, 5, or none
+        if num % 5 == 0 and num % 3 == 0:
             print("FizzBuzz")
             continue
-        elif i % 5 == 0:
+        elif num % 5 == 0:
             print("Buzz")
             continue
-        elif i % 3 == 0:
+        elif num % 3 == 0:
             print("Fizz")
             continue
-        print(i)
+        #If neither of these cases are hit, just print the number
+        print(num)
 main()
